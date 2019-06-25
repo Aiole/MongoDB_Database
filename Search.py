9a4410fa1cbb0aedfa124f278a11f8422500dcd5
Jack <jackwhershey@gmail.com>
2019-06-24 15:49:32 -1000

    Update Search.py

--- Search.py
+++ Search.py
@@ -1,3 +1,17 @@
+
+import pymongo
+
+
+#Setup creating DB and connecting to Mongo
+from pymongo import MongoClient
+client = MongoClient()
+
+client = MongoClient('localhost', 27017)
+
+
+db = client.ytla
+test_database = db.test_database
+
 print('Insert the time you are looking for: ')
 input_time = input()
 find_data = test_database.find_one({'timestamp': input_time})
