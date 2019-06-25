4f144d31ae6ace3e63027925f259c7eab72f8bc1
Jack <jackwhershey@gmail.com>
2019-06-25 09:18:40 -1000

    Update Search.py

--- Search.py
+++ Search.py
@@ -1,6 +1,4 @@
-
 import pymongo
-
 
 #Setup creating DB and connecting to Mongo
 from pymongo import MongoClient
@@ -12,7 +10,24 @@ client = MongoClient('localhost', 27017)
 db = client.ytla
 test_database = db.test_database
 
-print('Insert the time you are looking for: ')
-input_time = input()
-find_data = test_database.find_one({'timestamp': input_time})
-print(find_data)
+#Retrieval
+
+print("Please select a search method: 'Between', 'UptoPresent', ...") 
+
+
+#Accepting inputs in proper timestamp format
+print('Insert the start time you are looking for: ')
+start_time = input()
+print('Insert the end time you are looking for: ')
+end_time = input()
+
+
+#Sets the greater than or equal to and less than or equal to parameters with regards to start and end time
+gtequery = { "timestamp": { "$gte": start_time, "$lte": end_time } }
+
+#Searches database with those parameters
+gteresult = test_database.find(gtequery)
+
+#Prints all the results found
+for x in gteresult:
+	print(x)
