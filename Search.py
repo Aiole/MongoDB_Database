3323c4be48465c87995de5651f295224fbc2c1ac
Jack <jackwhershey@gmail.com>
2019-06-25 15:44:53 -1000

    Update Search.py

--- Search.py
+++ Search.py
@@ -9,27 +9,32 @@ client = MongoClient('localhost', 27017)
 db = client.ytla
 test_database = db.test_database
 
+
 #Retrieval
 
 
 #Choose desired search method
-print("Please select a search method: 'Between', 'UptoPresent', ...") 
-search_method = input()
 
-#Checks if the between method was entered
-between_search = {'Between', 'between', 'bet'}
-if search_method in between_search:
-	#Accepting inputs in proper timestamp format
-	print('Insert the start time you are looking for: ')
-	start_time = input()
-	print('Insert the end time you are looking for: ')
-	end_time = input() 
+
+def choose_search(search_method):
+
+	#Checks if the between method was entered
+	between_search = {'Between', 'between', 'bet'}
+	if search_method in between_search:
+		return 'Between'
+	 
+	#Checks if the upto method was entered
+	upto_search = {'upto', 'UpToPresent', 'uptopresent', 'UpTo', 'Upto'}
+	if search_method in upto_search:
+		return 'UpToPresent'
+
+
+
+def between_search_f(input_var,start_time,end_time):
 	
 	#Sets the greater than or equal to and less than or equal to parameters with regards to start 		and end time
 	gtequery = { "timestamp": { "$gte": start_time, "$lte": end_time } }	
 
-	print('Enter Variable(s) you would like to see (seperated by spaces): ')
-	input_var = input()
 	
 	#Checks for if the user wants all variables
 	all_vars = {'', ' ', 'all', 'All'}
@@ -52,19 +57,13 @@ if search_method in between_search:
 		for x in gteresult:
 			print(x)
 
-#Checks if the upto method was entered
-upto_search = {'upto', 'UpToPresent', 'uptopresent', 'UpTo', 'Upto'}
-if search_method in upto_search:
 
-	#Accepting input in proper timestamp format
-	print('Insert the start time you are looking for: ')
-	start_time = input() 
+def upto_search_f(input_var,start_time,end_time):
+
 	
 	#Sets the greater than or equal to parameter with regards to start time
 	gtequery = { "timestamp": { "$gte": start_time } }	
 
-	print('Enter Variable(s) you would like to see (seperated by spaces): ')
-	input_var = input()
 	
 	#Checks for if the user wants all variables
 	all_vars = {'', ' ', 'all', 'All'}
@@ -86,5 +85,4 @@ if search_method in upto_search:
 		#Prints all the results found
 		for x in gteresult:
 			print(x)
-
 
