bfe3ad4017033f1212bb1b45e38985bf938f092c
Jack <jackwhershey@gmail.com>
2019-07-05 11:13:42 -1000

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,24 +1,62 @@
+import pymongo
+import datetime
+import random
+import time
+
+
+#Setup creating DB and connecting to Mongo
+from pymongo import MongoClient
+client = MongoClient()
+
+client = MongoClient('localhost', 27017)
 
 db = client.ytla
 
 
 #Insertion 
-test_database = db.test_database
-
-
-array_1 = [0]*8
-array_2 = [0]*8
-array_3 = [0]*8
-array_4 = [0]*8
-array_5 = [0]*8
-array_6 = [0]*8
-array_7 = [0]*8
-array_8 = [0]*8
-i = 0
 
 
 while 1:
+	test_database = db.test_database
+	array_1=[0]*8
+	array_2=[0]*8
+	array_3=[0]*8
+	array_4=[0]*8
+	array_5=[0]*8
+	array_6=[0.0]*8
 
+	array_7=[0]*8
+	array_8=[0]*8
+	array_9=[0]*8
+	array_10=[0]*8
+	array_11=[0]*8
+	array_12=[0.0]*8
+
+	array_13=[0]*8
+	array_14=[0]*8
+	array_15=[0]*8
+	array_16=[0]*8
+	array_17=[0]*8
+	array_18=[0.0]*8
+
+	array_19=[0]*8
+	array_20=[0.0]*8
+
+
+
+	lf_Y = [0.0] * 14
+	lf_X = [0.0] * 14
+	lfI_X = [0.0] * 8
+	lfQ_X = [0.0] * 8
+	lfI_Y = [0.0] * 8
+	lfQ_Y = [0.0] * 8
+	iflo_x = [0.0] * 8
+	iflo_y = [0.0] * 8
+
+
+
+	i = 0
+	#Random floats and arrays generated
 	float_1 = random.randrange(1,10000) / 1000 
 	float_2 = random.randrange(1,10000) / 1000
 	float_3 = random.randrange(1,10000) / 1000
@@ -33,32 +71,54 @@ while 1:
 		array_6[i] = random.randrange(1,10000) / 1000
 		array_7[i] = random.randrange(1,10000) / 1000
 		array_8[i] = random.randrange(1,10000) / 1000
+		array_9[i] = random.randrange(1,10000) / 1000
+		array_10[i] = random.randrange(1,10000) / 1000
+		array_11[i] = random.randrange(1,10000) / 1000
+		array_12[i] = random.randrange(1,10000) / 1000
 		i+=1
 	
 	timenow = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.utcnow())
-	print(timenow)		
-	
+	print(timenow)
+		
+	#Creation of Document
 	data = {
 		'timestamp': timenow,
-		'float_1': float_1, 
-		'float_2': float_2,
-		'float_3': float_3,
-		'float_4': float_4,
-		'array_1': ', '.join(str(e) for e in array_1),
-		'array_2': ', '.join(str(e) for e in array_2),	
-		'array_3': ', '.join(str(e) for e in array_3),	
-		'array_4': ', '.join(str(e) for e in array_4),	
-		'array_5': ', '.join(str(e) for e in array_5),	
-		'array_6': ', '.join(str(e) for e in array_6),	
-		'array_7': ', '.join(str(e) for e in array_7),	
-		'array_8': ', '.join(str(e) for e in array_8)		
+		'nt_state': float_1, 
+		'nt_select': float_2,
+		'lo_freq': float_3,
+		'lo_power': float_4,
+		'sel1X': ', '.join(str(e) for e in array_1),
+		'sel2X': ', '.join(str(e) for e in array_2),	
+		'hybrid_selX': ', '.join(str(e) for e in array_3),	
+		'intswX': ', '.join(str(e) for e in array_4),	
+		'acc_lenX': ', '.join(str(e) for e in array_5),	
+		'intLenX': ', '.join(str(e) for e in array_6),	
+		'sel1Y': ', '.join(str(e) for e in array_7),
+		'sel2Y': ', '.join(str(e) for e in array_8),	
+		'hybrid_selY': ', '.join(str(e) for e in array_9),	
+		'intswY': ', '.join(str(e) for e in array_10),	
+		'acc_lenY': ', '.join(str(e) for e in array_11),	
+		'intLenY': ', '.join(str(e) for e in array_12),	
+		'acc_lenX': ', '.join(str(e) for e in array_13),	
+		'intLenX': ', '.join(str(e) for e in array_14),	
+		'sel1Y': ', '.join(str(e) for e in array_15),
+		'sel2Y': ', '.join(str(e) for e in array_16),	
+		'hybrid_selY': ', '.join(str(e) for e in array_17),	
+		'intswY': ', '.join(str(e) for e in array_18),	
+		'acc_lenY': ', '.join(str(e) for e in array_19),	
+		'intLenY': ', '.join(str(e) for e in array_20),
+		'lfI_X':', '.join(str(e) for e in lfI_X),
+		'lfQ_X':', '.join(str(e) for e in lfQ_X),
+        	'iflo_x':', '.join(str(e) for e in iflo_x),
+        	'iflo_y':', '.join(str(e) for e in iflo_y)
 	}
 
 
 
 
-
+	#Uploading documents to database 
 	result = test_database.insert_one(data)
 	time.sleep(10)
-	
+
+
 
