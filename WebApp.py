b4b9c1d1a539c28f9c897f3ac23d7d9c6be108b2
Jack <jackwhershey@gmail.com>
2019-07-25 15:25:35 -1000

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -1,6 +1,6 @@
 import os, flask
 from flask import Flask, render_template, request, redirect, url_for
-from Search import choose_search, between_search_f, upto_search_f, upto_time_f, between_time_f, query_type, float_parse, array_parse, csv_write, create_df, get_results, flo_names, arr_names, all_vars, not_var, create_plot, vars_notes
+from Search import choose_search, between_search_f, upto_search_f, upto_time_f, between_time_f, query_type, float_parse, array_parse, csv_write, create_df, get_results, flo_names, arr_names, all_vars, not_var, create_plot, vars_notes, key_search, save_list
 import csv
 import datetime
 from datetime import timedelta
@@ -11,64 +11,219 @@ import logging
 app = flask.Flask(__name__)
 
 #Homepage Display
-@app.route("/" , methods=['GET','POST'])
-def home():
-
-	if request.method == 'POST':
-		if request.form['action'] == 'Find': 
-			search_var = request.form['search_var']
-			search_vars = search_var.split()
-			print(search_vars)
-			every_var = vars_notes()
-			a = 1
-			for string in search_vars:
-				a = 1
-				while a < len(every_var):
-					print(string) 
-					print(every_var[a])
-					if string not in every_var[a]:
-						every_var.pop(a)
-						every_var.pop(a-1)
-						a-=2			
-		
-					a+=2
-	
-			a = 1
-			print(every_var)
-			while a < len(every_var):
-				every_var.pop(a)
-				a+=1
-
-			print (len(every_var))
-			if len(every_var) == 0:
-				every_var.insert(0, 'Search Results')
-				every_list = [every_var]
-				return render_template('uptopage_updated.html', var=every_list, note='No results found make your search less specific and check spelling')		
-				
-
-			every_var.insert(0, 'Search Results')
-			every_list = [every_var]
-			print(every_list)
-			return render_template('uptopage.html', var=every_list)		
+@app.route("/")
+def home():	
 		
 	return render_template('homepage.html')
   
+
+
+@app.route('/KeywordSearch', methods=['GET','POST'])
+def key():
+
+
+
+	if request.method == 'POST':
+
+		if request.form['action'] == 'Submit Query': 
+
+			search_var = request.form['search_var']
+			every_list = key_search(search_var)
+			save_list(search_var)
+			return render_template('keypage.html', var=every_list, ph = search_var)
+
+		else:
+			return render_template('keypage.html')
+
+	else:
+		return render_template('keypage.html')
+
+
+
+
+	
+
+@app.route('/KeywordSearch/<inputvar>', methods=['GET','POST'])
+def keyword(inputvar):
+
+
+	input_var = inputvar
+
+
+	lines = open('VariableNotes.csv').read().splitlines()
+	for x in lines:
+		if input_var in x:
+			notes = x.split(': ')[1]
+			break
+
+
+	lines = open('YAxis.csv').read().splitlines()
+	for x in lines:
+		if input_var in x:
+			yaxis = x.split(': ')[1]
+			break
+
+	lines = open('Titles.csv').read().splitlines()
+	for x in lines:
+		if input_var in x:
+			title = x.split(': ')[1]
+			break
+
+
+	search_var = open('Search.csv').read()
+	every_var = key_search(search_var)
+
+
+	if request.method == 'POST':
+
+
+		try:
+
+			if request.form['action'] == 'Submit Query': 
+				return render_template('keypage_updated.html', var=every_var, note=notes)
+
+
+			if request.form['action'] == 'Graph': 
+				start_time = request.form['start_time']
+
+	
+
+				#Sets the greater than or equal to parameter with regards to start time	
+				query = { "timestamp": { "$gte": start_time } }	
+
+				data_time = upto_time_f(query)
+				flo_data, arr_data = upto_search_f(input_var,query)
+				try:
+					data = create_df(input_var,flo_data,arr_data,data_time)
+					graph = create_plot(data,yaxis,title)
+
+				except ValueError:
+					notes = 'You must enter a time before graphing'
+					return render_template("keypage_updated.html", var=every_var, start=start_time, note=notes)
+
+
+				try:
+					return render_template("keypage_updated.html", plot=graph, var=every_var, start=start_time, note=notes)
+
+				except UnboundLocalError:
+					return render_template("keypage_updated.html", plot=graph, var=every_var, start=start_time)
+
+
+			if request.form['action'] == 'Last hour': 
+				starttime = datetime.datetime.utcnow()
+				starttime -= timedelta(hours = 1)
+				start_time = starttime.strftime("%Y-%m-%d %H:%M:%S")
+		
+				query = { "timestamp": { "$gte": start_time } }		
+
+				data_time = upto_time_f(query)
+				flo_data, arr_data = upto_search_f(input_var,query)
+				data = create_df(input_var,flo_data,arr_data,data_time)
+				graph = create_plot(data,yaxis,title)
+				try:
+					return render_template("keypage_updated.html", plot=graph, var=every_var, start=start_time, note=notes)
+
+				except UnboundLocalError:
+					return render_template("keypage_updated.html", plot=graph, var=every_var, start=start_time)
+
+
+			if request.form['action'] == 'Last 24 hours': 
+				starttime = datetime.datetime.utcnow()
+				starttime -= timedelta(hours = 24)
+				start_time = starttime.strftime("%Y-%m-%d %H:%M:%S")
+		
+				query = { "timestamp": { "$gte": start_time } }		
+
+				data_time = upto_time_f(query)
+				flo_data, arr_data = upto_search_f(input_var,query)
+				data = create_df(input_var,flo_data,arr_data,data_time)
+				graph = create_plot(data,yaxis,title)
+				try:
+					
+					return render_template("keypage.html", plot=graph, var=every_var, start=start_time, note=notes)
+
+				except UnboundLocalError:
+					return render_template("keypage.html", plot=graph, var=every_var, start=start_time)
+					
+
+
+
+
+
+			if request.form['action'] == 'Last 48 hours': 
+				starttime = datetime.datetime.utcnow()
+				starttime -= timedelta(hours = 48)
+				start_time = starttime.strftime("%Y-%m-%d %H:%M:%S")
+		
+				query = { "timestamp": { "$gte": start_time } }		
+
+				data_time = upto_time_f(query)
+				flo_data, arr_data = upto_search_f(input_var,query)
+				data = create_df(input_var,flo_data,arr_data,data_time)
+				graph = create_plot(data,yaxis,title)
+
+
+				try:
+					return render_template("uptopage_updated.html", plot=graph, var=every_var, start=start_time, note=notes)
+
+				except UnboundLocalError:
+					return render_template("uptopage_updated.html", plot=graph, var=every_var, start=start_time)
+
+
+			if request.form['action'] == 'Last 72 hours': 
+				starttime = datetime.datetime.utcnow()
+				starttime -= timedelta(hours = 72)
+				start_time = starttime.strftime("%Y-%m-%d %H:%M:%S")
+		
+				query = { "timestamp": { "$gte": start_time } }		
+
+				data_time = upto_time_f(query)
+				flo_data, arr_data = upto_search_f(input_var,query)
+				data = create_df(input_var,flo_data,arr_data,data_time)
+				graph = create_plot(data,yaxis,title)
+
+				try:
+					return render_template("uptopage_updated.html", plot=graph, var=every_var, start=start_time, note=notes)
+
+				except UnboundLocalError:
+					return render_template("uptopage_updated.html", plot=graph, var=every_var, start=start_time)
+
+
+
+			if request.form['action'] == 'Download to csv file': 
+		
+				start_time = request.form['start_time']
+		
+				query = { "timestamp": { "$gte": start_time } }	
+		
+		
+				csv_write(input_var,query)
+
+
+				return render_template('uptopage.html', var=every_var, note='File saved to /corr/home/dbDownloads')
+
+				
+
 		
 
-
-'''#Between Page Display
-@app.route('/Up2Present')
-def upto2():
-
-	search_vars = search_var.split()
-	every_var = all_vars_no_cata()
-	print(every_var)
-	for string in search_vars:
-		for x in every_var:
-			if string not in x:
-				every_var.remove(x)
 	
-	return render_template('uptopage.html', var=every_var)'''
+				
+			
+
+		except KeyError:
+			
+			if request.form['inputvar'] != '':
+				input_var = request.form['inputvar']
+				print(input_var)
+				
+				
+			print('here4')
+			return render_template('keypage_updated.html', var=every_var, note=notes)
+
+	
+
+	return render_template('keypage_updated.html', var=every_var, note=notes)
+
 
 
 #Between Page Display
@@ -179,7 +334,7 @@ def upto_graph(inputvar):
 
 	every_var = all_vars()
 	input_var = inputvar
-	print(every_var)	
+	#print(every_var)	
 	
 	if not_var(input_var,every_var):
 		return render_template("uptopage_updated.html", var=every_var, note='This variable is not in the Database')
